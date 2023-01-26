ebe7b2626b9ebbb224c74de156e187adf4793f84
helena <helena.gilja@gmail.com>
2023-01-26 22:39:50 +0000

    api key added into script js and initial query url with the example added

---
+++ final/js/script.js
@@ -0,0 +1,12 @@
+// API Key
+let apiKey = "9ed27dc20b6878421a84136c5fe9e945";
+
+// create variables fo parameters
+// city
+//country
+// different url for future forecast?
+
+// URL Build
+let queryURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" +
+  apiKey;
