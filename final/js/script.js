9d97f327dce87920ac581634f7dc5ab1f85f2568
helena <helena.gilja@gmail.com>
2023-01-27 15:05:07 +0000

    chained two data fetches, tested in console and working

--- final/js/script.js
+++ final/js/script.js
@@ -1,7 +1,7 @@
 // API Key
 let apiKey = "9ed27dc20b6878421a84136c5fe9e945";
 
-// create variables fo parameters
+// create variables for parameters
 // city
 //country
 // different url for future forecast?
@@ -11,18 +11,23 @@ let queryURL1 =
   "https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" +
   apiKey;
 
-let queryURL2 =
-  "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=" +
-  apiKey;
-
+// we need this data in order to make the 2nd request
 fetch(queryURL1)
   .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+  .then((citiesFound) => {
+    let firstCity = citiesFound[0];
+    console.log(firstCity.lat);
+    console.log(firstCity.lon);
 
-fetch(queryURL2)
+    let queryURL2 =
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=` +
+      apiKey;
+
+    return fetch(queryURL2);
+  })
+
   .then((response) => response.json())
   .then((data) => {
+    // the below is the data from return fetch
     console.log(data);
   });
