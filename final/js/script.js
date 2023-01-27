e256746950728378382d4614b522b082266d2027
helena <helena.gilja@gmail.com>
2023-01-27 15:55:34 +0000

    event listener added to searchButton, also created dynamic variables, tested and working

--- final/js/script.js
+++ final/js/script.js
@@ -6,28 +6,43 @@ let apiKey = "9ed27dc20b6878421a84136c5fe9e945";
 //country
 // different url for future forecast?
 
-// URL Build
-let queryURL1 =
-  "https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" +
-  apiKey;
+/// put the below in the eventListener for search button
+/// add a new button below for each city from the search
+/// grab the text of the city from search button
 
-// we need this data in order to make the 2nd request
-fetch(queryURL1)
-  .then((response) => response.json())
-  .then((citiesFound) => {
-    let firstCity = citiesFound[0];
-    console.log(firstCity.lat);
-    console.log(firstCity.lon);
+let searchButton = document.querySelector("#search-button");
+let searchInput = document.querySelector("#search-input");
 
-    let queryURL2 =
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=` +
-      apiKey;
+searchButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  // URL Build
+  let city = searchInput.value;
+  console.log(city);
 
-    return fetch(queryURL2);
-  })
+  // let city = "London";
 
-  .then((response) => response.json())
-  .then((data) => {
-    // the below is the data from return fetch
-    console.log(data);
-  });
+  let queryURL1 =
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=` +
+    apiKey;
+
+  // we need this data in order to make the 2nd request
+  fetch(queryURL1)
+    .then((response) => response.json())
+    .then((citiesFound) => {
+      let firstCity = citiesFound[0];
+      console.log(firstCity.lat);
+      console.log(firstCity.lon);
+
+      let queryURL2 =
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=` +
+        apiKey;
+
+      return fetch(queryURL2);
+    })
+
+    .then((response) => response.json())
+    .then((data) => {
+      // the below is the data from return fetch
+      console.log(data);
+    });
+});
