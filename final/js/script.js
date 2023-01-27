9d49ad94ee4f6266550a3d681d2449d33a447e3e
helena <helena.gilja@gmail.com>
2023-01-27 14:41:22 +0000

    2 url's built for testing, one with geo and the other with lat and lon

--- final/js/script.js
+++ final/js/script.js
@@ -7,12 +7,22 @@ let apiKey = "9ed27dc20b6878421a84136c5fe9e945";
 // different url for future forecast?
 
 // URL Build
-let queryURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" +
+let queryURL1 =
+  "https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" +
   apiKey;
 
-fetch(queryURL)
+let queryURL2 =
+  "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=" +
+  apiKey;
+
+fetch(queryURL1)
   .then((response) => response.json())
-  .then(function (result) {
-    console.log(result);
+  .then((data) => {
+    console.log(data);
+  });
+
+fetch(queryURL2)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
   });
