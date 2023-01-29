b9d9465981301f9365668c56a17277a371069914
helena <helena.gilja@gmail.com>
2023-01-29 17:27:19 +0000

    more comments added throughout the code for betetr understanding

--- final/js/script.js
+++ final/js/script.js
@@ -192,6 +192,9 @@ function forecastSearch(apiKey, cityName, weatherForecast) {
       const filteredList = response5Day.list.filter(function (item) {
         return item.dt_txt.endsWith("12:00:00");
       });
+
+      console.log("filteredList", filteredList);
+
       renderForecast(filteredList, weatherForecast, response5Day);
     });
 }
